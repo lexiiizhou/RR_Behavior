c78dc2f644d9957640f8fcf0c39d480f510d9dac
Lexi Zhou <lexizczh@berkeley.edu>
2021-09-04 16:21:23 -0700

    ongoing

--- classes.py
+++ classes.py
@@ -2,9 +2,7 @@ import numpy
 import pandas
 
 
-class Event:
-    """An event in a session"""
-
+class BonsaiEvent:
     def __init__(self, event):
         self.item = self
         self.event_description = event[0]
@@ -16,66 +14,43 @@ class Event:
         self.next = None
 
 
-class Block:
-    """A block of events defined by the direction of movement"""
+class Trial:
+    """
+    Series of bonsai events in the same restaurant stored as
+    a doubly linked list
+    """
 
-    def __init__(self, block):
-        self.counterclockwise = False
+    def __init__(self, dll_of_bonsaievents, index):
+        """
+        events -- DLL: list of bonsai_event objects
+        """
+        self.valid = False  # True: Valid Trial
+        self.tone_prob
+        self.restaurant
+        self.choice
+        self.index
+        self.current = self
+        self.prev
+        self.next
+
+
+class Lap:
+    """A doubly linked list of trials"""
+
+    def __init__(self, dll_of_trials, direction, index):
+        self.direction = direction
+        self.index = index
+        self.total_trial_count = dll_of_trials.size
+        self.valid_trial_count
+        self.current = self
+        self.prev
+        self.next
 
 
 class Session:
-    """Where trials and intermissions exist"""
+    """A doubly linked list of laps"""
 
-    def __init__(self, events_list):
-        """
-        Create a session
-        events_list -- List: a chronological list of events and timestamps [event, timestamp, event_code]
-        """
-        self.trial_count  # How many trials are in this session
-        self.intermission_count  # How many intermissions are in this session
-        self.events_list = events_list
-        self.headEvent = None  # First event in the session
-
-    def parse_event(self):
-
-        pass
-
-    def add_next_event(self, event, event_type):
-        if event_type == 'Trial':
-            newTrial = Trial()
-
-        pass
-
-    def event_count(self):
-        return self.trial_count + self.intermission_count
-
-
-class Trial(Block):
-    """
-
-    """
-
-    type = 'Trial'
-
-    def __init__(self, events):
-        self.tone_prob
-        self.restaurant
-        self.choice  # True: Accept | False: reject
-        self.re
-        """Keep a log of only trials"""
-        self.prev_trial
-        self.next_trial
-
-    def process_time_stamps(self):
-        return
-
-    def add_prev_trial(self):
-        pass
-
-
-class Intermission(Block):
-    type = 'Intermission'
-
-    def __init__(self, event_log):
-        self.event_log = event_log
+    def __init__(self, dll_of_laps):
+        self.lap_count = dll_of_laps.size
+        self.valid_trial_count
 
